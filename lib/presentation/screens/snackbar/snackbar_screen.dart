0f04a80327cf5a1d52acfa14718db3d4bdb3ce0d
PatrickLuisM <patricklmo32@gmail.com>
2024-02-19 01:59:02 -0500

    first commit

---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SnackBarScreen extends StatelessWidget {
+  const SnackBarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
